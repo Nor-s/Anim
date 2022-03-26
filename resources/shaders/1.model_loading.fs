b698b71458cf527a33ce2970227edb3fea60ea95
Nor-s <s28118149@gmail.com>
2022-03-26 15:20:11 +0900

    Add skybox textures

--- resources/shaders/1.model_loading.fs
+++ resources/shaders/1.model_loading.fs
@@ -4,10 +4,6 @@ out vec4 FragColor;
 in vec2 TexCoords;
 
 uniform sampler2D texture_diffuse1;
-uniform sampler2D texture_diffuse2;
-uniform sampler2D texture_diffuse3;
-uniform sampler2D texture_specular1;
-uniform sampler2D texture_specular2;
 
 void main()
 {    

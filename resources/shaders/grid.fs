46259e8bf3f8e5825fa3d520af03e414c6f0808a
nsg <40836757+Nor-s@users.noreply.github.com>
2022-08-11 17:37:56 +0900

    Change grid color

--- resources/shaders/grid.fs
+++ resources/shaders/grid.fs
@@ -39,39 +39,26 @@ float LinearizeDepth(float depth)
     return (2.0 * near * far) / (far + near - z * (far - near))/far;
 }
 
+// https://madebyevan.com/shaders/grid/
+float get_grid(vec2 xz, float size) {
+	vec2 uv = xz/size;
+  	vec2 grid = abs(fract(uv - 0.5) - 0.5)/ fwidth(uv);
+    return 1.0 -smoothstep(0.0, width,  min(grid.x, grid.y));
+}
+vec2 get_axis(vec2 xz) {
+	vec2 uv = xz;
+  	vec2 axis = abs(uv)/ fwidth(uv);
+    return 1.0 -smoothstep(0.0, width, axis);
+}
 void main() {
 	float t = -near_vec.y / (far_vec.y-near_vec.y);
 	vec3 R = near_vec + t * (far_vec-near_vec);
 	vec4 bg_color = vec4(vec3(0.3),0.5);
 	vec4 grid_color = vec4(vec3(0.6), 0.7);
-	vec4 x_color = vec4(1.0, 0.3, 0.3, 0.7);
-	vec4 z_color = vec4(0.0706, 0.2471, 0.6235, 0.7);
+	vec4 grid_color2 = vec4(vec3(0.95), 0.7);
 
-
-	// https://madebyevan.com/shaders/grid/
-	vec2 uv = R.xz/scale;
-  	float half_scale = scale / 2.0;
-  	vec2 grid = abs(fract(uv - 0.5) - 0.5)/ fwidth(uv);
-  	vec2 axis = abs(uv)/ fwidth(uv);
-    float ratio = 1.0 -smoothstep(0.0, width,  min(grid.x, grid.y));
-
-	// grid color
-	color = bg_color;
-	color.a *= ratio; 
-	color.rgb = mix(color.rgb, grid_color.rgb, ratio);
-
-	// axes color
-    vec2 axes = 1.0 -smoothstep(0.0, width, axis);
-	color.rgb =  mix(color.rgb, x_color.rgb, axes.y);
-	color.rgb =  mix(color.rgb, z_color.rgb, axes.x);
-	color.a = max(color.a, axes.x*x_color.a);
-	color.a = max(color.a, axes.y*z_color.a);
-
-	// angle fading
-	vec3 camera_pos = vec3(inverse(view) * vec4(0.0, 0.0, 0.0, 1.0));
-	vec3 view_dir = (camera_pos - R);
-	float dist = length(view_dir);
-	float angle_fading =  abs(view_dir.y/dist);
+	vec4 x_color = vec4(0.9569, 0.3686, 0.3686, 1.0);
+	vec4 z_color = vec4(0.0549, 0.6235, 1.0, 1.0);
 
 	// set depth
 	gl_FragDepth = computeDepth(R);
@@ -80,5 +67,34 @@ void main() {
 	float linear_depth = LinearizeDepth(gl_FragDepth);
     float depth_fading = 1.0-smoothstep(0.0, 1., linear_depth);
 
-	color.a *= angle_fading * depth_fading;
+	// angle fading
+	vec3 camera_pos = vec3(inverse(view) * vec4(0.0, 0.0, 0.0, 1.0));
+	vec3 view_dir = (camera_pos - R);
+	float dist = length(view_dir);
+	float angle_fading =  abs(view_dir.y/dist);
+
+	// grid color	
+	// https://madebyevan.com/shaders/grid/
+	float grid_ratio = get_grid(R.xz, scale);
+	color = bg_color;
+	color.rgb = mix(color.rgb, grid_color.rgb, grid_ratio);
+
+	float grid_ratio1 = get_grid(R.xz, scale*10);
+	color = bg_color;
+	color.rgb = mix(color.rgb, grid_color2.rgb, grid_ratio1* (1.0 - depth_fading));
+
+	// axes color
+    vec2 axes = get_axis(R.xz);
+	color.rgb =  mix(color.rgb, x_color.rgb, axes.y);
+	color.rgb =  mix(color.rgb, z_color.rgb, axes.x);
+
+	// alpha
+	angle_fading = min(1.0, max(angle_fading, angle_fading + 0.3));
+	color.a *= grid_ratio; 
+	color.a = max(color.a, axes.x*x_color.a);
+	color.a = max(color.a, axes.y*z_color.a);
+	// color = vec4(angle_fading);
+	color.a *=  angle_fading*depth_fading;
+
+
 }

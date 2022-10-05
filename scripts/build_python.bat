d9d8e6313ba599f17686c319ea2bb7c5a4ad38cc
nsg <40836757+Nor-s@users.noreply.github.com>
2022-10-05 23:11:09 +0900

    Add ROMP setting script, test ipynb

--- scripts/build_python.bat
+++ scripts/build_python.bat
@@ -17,11 +17,19 @@ if exist %mypath%\..\python\python.exe (
 
      echo -- START::LAYOUT    
      
-           %cpython_path%\PCBuild\amd64\python.exe %cpython_path%PC\layout\main.py -b %cpython_path%\PCBuild\amd64  -s %cpython_path% -t %cpython_path%\temp\  --include-underpth --include-stable --include-pip --include-dev --include-launchers  --include-venv --include-symbols --copy %install_path%
+           %cpython_path%\PCBuild\amd64\python.exe %cpython_path%PC\layout\main.py -b %cpython_path%\PCBuild\amd64  -s %cpython_path% -t %cpython_path%\temp\  --preset-default --include-underpth --include-stable --include-pip --include-dev --include-launchers  --include-venv --include-symbols --copy %install_path%
 
      echo -- END::LAYOUT 
      echo -- START::PIP INSTALL 
-          %install_path%\python.exe -m pip install mediapipe==0.8.10.1
-          %install_path%\python.exe -m pip  install PyGLM==2.5.7
+          %install_path%\python.exe -m pip install --upgrade pip
+          @REM %install_path%\python.exe -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+          %install_path%\python.exe -m pip install --upgrade setuptools numpy cython
+          %install_path%\python.exe -m pip install --upgrade simple-romp
+          %install_path%\python.exe -m pip install onnxruntime 
+          @REM TODO: bpy /2.82 (버전)폴더를 python에 옮겨야함
+          @REM %install_path%\python.exe -m pip install future-fstrings mathutils==2.81.2
+          @REM %install_path%\python.exe -m pip install /path/to/downloaded/bpy-2.91a0-cp37-cp37m-manylinux2014_x86_64.whl && bpy_post_install       
+          @REM ../../../python/scripts/romp --mode=video -t -sc=2  --show_largest --onnx -i=C:/Users/No/Downloads/cxk.mp4 -o=C:/Users/No/Downloads/temp1
+          @REM ../../../python/python.exe tools/convert2fbx.py --input=C:/Users/No/Downloads/temp1/video_results.npz --output=C:/Users/No/Downloads/temp1/video_results3.fbx --gender=female
      echo -- END::PIP INSTALL 
 )

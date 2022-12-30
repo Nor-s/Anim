11f7915647f99bdb99b59f9f6761c8c4e947646a
Nor-s <s28118149@gmail.com>
2022-12-31 02:53:21 +0900

    test mrt on ma

--- src/graphics/opengl/framebuffer.cpp
+++ src/graphics/opengl/framebuffer.cpp
@@ -118,72 +118,6 @@ namespace anim
     }
     void Framebuffer::init_framebuffer_with_MSAA()
     {
-        // configure MSAA framebuffer
-        // --------------------------
-        glGenFramebuffers(1, &FBO_);
-        glBindFramebuffer(GL_FRAMEBUFFER, FBO_);
-        // create a multisampled color attachment texture
-        glGenTextures(1, &msaa_texture_id_);
-        glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, msaa_texture_id_);
-        glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples_, GL_RGBA, width_, height_, GL_TRUE);
-        glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
-
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, msaa_texture_id_, 0);
-
-        glGenTextures(1, &msaa_texture2_id_);
-        glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, msaa_texture2_id_);
-        glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples_, GL_R32I, width_, height_, GL_TRUE);
-        glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
-
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + 1, GL_TEXTURE_2D_MULTISAMPLE, msaa_texture2_id_, 0);
-        std::vector<GLenum> draw_buffers = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
-        glDrawBuffers(draw_buffers.size(), draw_buffers.data());
-
-        // create a (also multisampled) renderbuffer object for depth and stesncil attachments
-        glGenRenderbuffers(1, &d24s8_RBO_);
-        glBindRenderbuffer(GL_RENDERBUFFER, d24s8_RBO_);
-        glRenderbufferStorageMultisample(GL_RENDERBUFFER, samples_, GL_DEPTH24_STENCIL8, width_, height_);
-        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, d24s8_RBO_);
-        glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-        {
-#ifndef NDEBUG
-            std::cout << "ERROR::FRAMEBUFFER::MSAA Framebuffer is not complete!" << std::endl;
-#endif
-            is_error_ = true;
-        }
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-        // configure second post-processing framebuffer
-        glGenFramebuffers(1, &intermediate_FBO_);
-        glBindFramebuffer(GL_FRAMEBUFFER, intermediate_FBO_);
-        // create a color attachment texture
-        glGenTextures(1, &screen_texture_id_);
-        glBindTexture(GL_TEXTURE_2D, screen_texture_id_);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width_, height_, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, screen_texture_id_, 0); // we only need a color buffer
-
-        uint32_t screen_texture2_id;
-        glGenTextures(1, &screen_texture2_id);
-        glBindTexture(GL_TEXTURE_2D, screen_texture2_id);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_R32I, width_, height_, 0, GL_RED_INTEGER, GL_UNSIGNED_BYTE, NULL);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, screen_texture2_id, 0);
-        glDrawBuffers(draw_buffers.size(), draw_buffers.data());
-
-        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-        {
-#ifndef NDEBUG
-
-            std::cout << "ERROR::FRAMEBUFFER:: Intermediate framebuffer is not complete!" << std::endl;
-#endif
-            is_error_ = true;
-        }
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
     }
 
     void Framebuffer::set_quad_VAO()
@@ -227,30 +161,10 @@ namespace anim
         glStencilMask(~0); // https://community.khronos.org/t/how-to-clear-stencil-buffer-after-stencil-test/15882/4
         glDisable(GL_SCISSOR_TEST);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-        // glDisable(GL_STENCIL_TEST);
         glDisable(GL_STENCIL_TEST);
-        // glStencilMask(0x00);
-        // glStencilFunc(GL_EQUAL, 1, 0xFF);
-        // glStencilOpSeparate(GL_FRONT, GL_KEEP, GL_KEEP, GL_KEEP);
     }
     void Framebuffer::unbind()
     {
-        if (is_msaa_)
-        {
-            // glBindFramebuffer(GL_READ_FRAMEBUFFER, FBO_);
-            // glReadBuffer(GL_COLOR_ATTACHMENT1);
-            // glBindFramebuffer(GL_DRAW_FRAMEBUFFER, intermediate_FBO_);
-            // glDrawBuffer(GL_COLOR_ATTACHMENT1);
-
-            // glBlitFramebuffer(0, 0, width_, height_, 0, 0, width_, height_, GL_COLOR_BUFFER_BIT, GL_NEAREST);
-
-            // glBindFramebuffer(GL_READ_FRAMEBUFFER, FBO_);
-            // glReadBuffer(GL_COLOR_ATTACHMENT0);
-            // glBindFramebuffer(GL_DRAW_FRAMEBUFFER, intermediate_FBO_);
-            // glDrawBuffer(GL_COLOR_ATTACHMENT0);
-
-            // glBlitFramebuffer(0, 0, width_, height_, 0, 0, width_, height_, GL_COLOR_BUFFER_BIT, GL_NEAREST);
-        }
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
     }
     bool Framebuffer::error()
